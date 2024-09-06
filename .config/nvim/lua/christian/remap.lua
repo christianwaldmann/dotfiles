44e06ec85c8bed6bd1853ec0879b2619804acfc3
christianwaldmann <christianwaldmann>
2024-09-06 21:40:13 +0200

    remap esc to jj in vim

--- .config/nvim/lua/christian/remap.lua
+++ .config/nvim/lua/christian/remap.lua
@@ -26,8 +26,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
